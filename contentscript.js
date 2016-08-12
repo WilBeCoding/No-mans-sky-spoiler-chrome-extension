f7a5dc6fa66bb4a863ad4f8635bd1682a84b9a52
Wilson Rondini <wilbecoding@gmail.com>
2016-08-11 18:10:53 -0600

    Simple MPV no styling just alert

--- contentscript.js
+++ contentscript.js
@@ -1,55 +1,53 @@
 var spoilers = [
-  // "center of the universe",
-  // "beat",
-  // "ending",
-  // "ended",
-  // "complete",
-  // "completed",
-  // "the story was",
-  // "the game was",
-  // "it was",
-  // "didn't like",
-  // "liked",
-  // "No man's sky was",
-  // "ships"
-  "wolf"
+  "center of the universe",
+  "beat the game",
+  "the games ending",
+  "game ended",
+  "completed the game",
+  "finished the game",
+  "no man's sky story was",
+  "no mans sky story was",
+  "no man's sky was",
+  "it was",
+  "didn't like no man's sky",
+  "didn't like no mans sky",
+  "liked",
+  "No man's sky was",
 ];
+
+// To-Do:
+// -Make all letters lowercase before looping through the content
+// -Add a pretty modal
 
 console.log("Looking for spoilers");
 
 var counter = 0;
 
-console.log(document.body.innerText.indexOf("wolf"),  '     index of tracker');
-
-console.log(spoilers,  '   spoilers');
-
 $(document).ready(function(){
   for(var i = 0; i < spoilers.length; i++) {
-    console.log(spoilers[i], '     spoilers i')
     if(document.body.innerText.indexOf(spoilers[i]) > -1) {
       counter++;
-      console.log("FIRST if STATEMENT HITS")
-      console.log(counter + '    counter');
     }
   }
   if(counter > 0) {
     console.log("There are potential spoilers on this page");
-    $( function() {
-      $('body,html').dialog({
-        resizable: false,
-        height: "auto",
-        width: 400,
-        modal: true,
-        buttons: {
-          "Delete all items": function() {
-            $( this ).dialog( "close" );
-          },
-          Cancel: function() {
-            $( this ).dialog( "close" );
-          }
-        }
-      });
-    } );
+    alert("There are potential spoilers on this page!");
+    // $( function() {
+    //   $('body,html').dialog({
+    //     resizable: false,
+    //     height: "auto",
+    //     width: 400,
+    //     modal: true,
+    //     buttons: {
+    //       "Delete all items": function() {
+    //         $( this ).dialog( "close" );
+    //       },
+    //       Cancel: function() {
+    //         $( this ).dialog( "close" );
+    //       }
+    //     }
+    //   });
+    // } );
   }else{
     console.log("This site is spoiler free")
   }
